779d86863542cd0874b3e89394eaafa9273002f2
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-06-04 13:56:47 -0700

    no_dups is passing tests

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,15 @@
 def no_dups(s):
     # Your code here
+    cache = {}
+    new_string = ""
+    s = s.split()
+    for word in s:
+        if word not in cache:
+            cache[word] = True
+            new_string += word + " "
+        
 
+    return new_string.strip()
 
 
 if __name__ == "__main__":
